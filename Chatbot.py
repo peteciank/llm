5a29c37b9663e0868a701ce59616331c08256b55
peteciank <106826936+peteciank@users.noreply.github.com>
2023-12-28 12:33:47 -0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -4,8 +4,9 @@ import streamlit as st
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    "[View the source code](https://github.com/peteciank/llm)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/peteciank/llm?quickstart=1)"
+    "[View my LinkedIn Profile](https://www.linkedin.com/in/pedrociancaglini/)"
 
 st.title("💬 Chatbot")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
